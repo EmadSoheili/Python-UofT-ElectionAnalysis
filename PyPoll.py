e01917d113d85e0177a19600c90783c357f99e12
Emad <emad.soheili@gmail.com>
2022-09-08 15:09:52 -0400

    Change OS

--- PyPoll.py
+++ PyPoll.py
@@ -2,23 +2,46 @@
 # PyPoll with Python Project
 # University of Toronto - Data Analytics Boot Camp - Module 1 - PyPoll with Python
 
-# Read the data ================================================================================
+# Import Modules ================================================================================
 import csv
 import os
 
-    # Assign input and output file paths to variables ---------------
+# Set file paths ================================================================================
+
+    # Assign input and output file paths to variables 
 file_to_load = os.path.join("Resources","election_results.csv")
 file_to_save = os.path.join("Analysis","electionAnalysis.txt")
 
-    # Open the election file ---------------
+# Initialize some variables ================================================================================
+
+        # Add a accumulator and set it to zero
+total_votes = 0
+
+        # Create an empty list for candidates' names
+candidate_options = []
+
+# Read the data ================================================================================
+
+    # Open the election file 
 with open(file_to_load) as election_data:
 
-    # Read file ---------------
+    # Read file 
     file_reader = csv.reader(election_data)
 
-    # Read headers ---------------
+    # Read headers 
     headers = next(file_reader)
-    print(headers)
+
+# Analyze the data ================================================================================
+
+    # Go through every single rows
+    for row in file_reader:
+
+    # Count total votes
+        total_votes += 1
+
+# Print the analysis output ================================================================================
+
+print(total_votes)
 
 
 
